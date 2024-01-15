4a73e9bedf3e0d7cfac677b4536d264e2c769595
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-01-15 13:27:39 +0530

    Update About.tsx

--- src/components/Subject/About.tsx
+++ src/components/Subject/About.tsx
@@ -147,11 +147,9 @@ const About: React.FC<AboutProps> = ({
               {user && (
                 <Link href={`${router.query.subject}/submit`}>
                   <Button mt={2} mb={3} height="30px" width="250px">
-                    Ask Anything!
+                    Ask or Share Anything!
                   </Button>
-                   <Button mt={2} mb={3} height="30px" width="250px">
-                  Share Anything!
-                  </Button>
+                  
                 </Link>
             
               )}
