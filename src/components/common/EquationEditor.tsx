c35df11d5387ba4b99dd0509500dc7963eb9bcc0
devbinary003 <binarydata.sale@gmail.com>
2024-02-01 12:49:40 +0530

    fixed equation editor changes upload
    fixed equation editor changes upload

--- src/components/common/EquationEditor.tsx
+++ src/components/common/EquationEditor.tsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
-//import { addStyles, EditableMathField } from 'react-mathquill';
+//import {EditableMathField } from 'react-mathquill';
 import TexSymbols from '../common/textSymbols';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 
 //addStyles();
-const EditableMathField = dynamic(() => import('react-mathquill') as any, { ssr: false });
+//const EditableMathField = dynamic(() => import('react-mathquill') as any, { ssr: false });
+const EditableMathField = dynamic(
+  () => import("react-mathquill").then((mod) => mod.EditableMathField),
+  { ssr: false }
+);
 
 const EquationEditor = ({onInputChange}:any) => {
   const [formula, setFormula] = useState('');
@@ -28,9 +32,9 @@ const EquationEditor = ({onInputChange}:any) => {
       <Head>
       <script src="https://styleguide.brainly.com/images/math-symbols-icons-2936684655.js"></script>
       </Head>
-      {typeof window !== 'undefined' && (
+      {(typeof window !== 'undefined') &&
         <EditableMathField latex={formula} onChange={handleChange} />
-      )}
+      }
       <hr />
       <TexSymbols onClick={handleInsertSymbol} />
     </>
