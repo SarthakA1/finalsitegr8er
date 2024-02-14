6aa5e781e093deeca6333ba35b7a105d25585558
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-02-14 23:04:09 +0530

    Update PostItem.tsx

--- src/components/Posts/PostItem.tsx
+++ src/components/Posts/PostItem.tsx
@@ -251,7 +251,7 @@ const PostItem:React.FC<PostItemProps> = ({
 
             <Flex direction="column">
            <Flex direction="row">
-               <span style={{background: "#000000", color: "#fff", padding: "5px 10px 5px 10px", borderRadius: "15px", marginRight: "5px", fontSize: "12px"}}>
+              
                  <Text ml={2} fontSize='13pt' color="#2596be" fontWeight={600} mb={1}>MYP  </Text>
             {post.grade && (
                 <>
@@ -260,7 +260,7 @@ const PostItem:React.FC<PostItemProps> = ({
                     </Text>
                 </>
             )}
-                    </span>
+                
              </Flex>
             <Text fontSize='13pt' fontWeight={600} mb={1}> {post.title} </Text>
             {/* 
