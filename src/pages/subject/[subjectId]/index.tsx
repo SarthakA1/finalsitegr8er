9575feea66d191144548d37f190504b76b2c2078
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-02-20 10:23:36 +0530

    Update index.tsx

--- src/pages/subject/[subjectId]/index.tsx
+++ src/pages/subject/[subjectId]/index.tsx
@@ -40,26 +40,7 @@ const SubjectPage: React.FC<SubjectPageProps> = ({ subjectData }) => {
       window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 
-  const [isVisible, setIsVisible] = useState(false);
-
-  const handleScroll = () => {
-    // Show the button when the user scrolls down
-    if (window.scrollY > 100) {
-      setIsVisible(true);
-    } else {
-      setIsVisible(false);
-    }
-  };
-
-  useEffect(() => {
-    // Add scroll event listener when the component mounts
-    window.addEventListener('scroll', handleScroll);
-
-    // Remove the event listener when the component unmounts
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+ 
 
 
   return (
