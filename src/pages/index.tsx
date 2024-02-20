64236cd5199ae41077d6280f34dd2083b768e03b
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-02-20 09:54:35 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -110,26 +110,6 @@ const Home: NextPage = () => {
     setLoading(false);
   };
 
-  const [isVisible, setIsVisible] = useState(false);
-
-  const handleScroll = () => {
-    // Show the button when the user scrolls down
-    if (window.scrollY > 100) {
-      setIsVisible(true);
-    } else {
-      setIsVisible(false);
-    }
-  };
-
-  useEffect(() => {
-    // Add scroll event listener when the component mounts
-    window.addEventListener('scroll', handleScroll);
-
-    // Remove the event listener when the component unmounts
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
   const getUserPostVotes = async () => {
     try {
@@ -422,9 +402,9 @@ const Home: NextPage = () => {
       <Stack spacing={5}>
         <Recommendations />
         <button
-        className={`fixed bottom-0 right-0 bg-black rounded-s-full px-4 py-2 mr-6 mb-[71px] z-50 items-center text-xs flex gap-2 scrollToTopButton ${isVisible ? 'visible' : ''}`}
-         
+        className={`fixed bottom-0 right-0 bg-black rounded-s-full px-4 py-2 mr-6 mb-[71px] z-50 items-center text-xs flex gap-2`}
         onClick={scrollToTop}
+          
       >
         BACK TO TOP
        
