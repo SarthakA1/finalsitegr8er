c514161bd4c08e6543ceba53d7b062465d80a48f
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-02-24 14:03:19 +0530

    Update AnswerInput.tsx

--- src/components/Posts/Answers/AnswerInput.tsx
+++ src/components/Posts/Answers/AnswerInput.tsx
@@ -28,7 +28,7 @@ const AnswerInput:React.FC<AnswerInputProps> = ({ answerText, setAnswerText, use
             <Textarea
               value={answerText}
               onChange={(event) => setAnswerText(event.target.value)}
-              placeholder="What is the Answer to this Question?"
+placeholder={post.typeOfQuestions && post.typeOfQuestions.value === 'Resource' ? 'Write a Comment' : 'What is the Answer to this Question?'}
               fontSize="10pt"
               borderRadius={4}
               minHeight="160px"
