d93b82fe0f5d21215868c53e5dd5c3cc65ddac9f
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-02-24 13:46:41 +0530

    Update AnswerReplyInput.tsx

--- src/components/Posts/Answers/Reply/AnswerReplyInput.tsx
+++ src/components/Posts/Answers/Reply/AnswerReplyInput.tsx
@@ -24,61 +24,59 @@ const AnswerReplyInput: React.FC<AnswerReplyInputProps> = ({ answerText, setAnsw
     }
 
     return (
-        <>
-            <Flex direction="column" position="relative" className='sub-reply-input'>
-                {user ? (
-                    <>
-                        <Textarea
-                            value={answerText}
-                            onChange={(event) => setAnswerText(event.target.value)}
-                            placeholder="What would you like to reply?"
-                            fontSize="10pt"
-                            borderRadius={4}
-                            minHeight="160px"
-                            pb={10}
-                            _placeholder={{ color: "gray.500" }}
-                            _focus={{
-                                outline: "none",
-                                border: "1px solid black",
-                                bg: "white"
-                            }}
-                        />
-                        <Flex
-                            left="1px"
-                            right={0.1}
-                            bottom="1px"
-                            justify="flex-end"
-                            bg="gray.100"
-                            p="6px 8px"
-                            borderRadius="0px 0px 4px 4px"
+        <Flex direction="column" position="relative" className='sub-reply-input'>
+            {user ? (
+                <>
+                    <Textarea
+                        value={answerText}
+                        onChange={(event) => setAnswerText(event.target.value)}
+                        placeholder="What would you like to reply?"
+                        fontSize="10pt"
+                        borderRadius={4}
+                        minHeight="160px"
+                        pb={10}
+                        _placeholder={{ color: "gray.500" }}
+                        _focus={{
+                            outline: "none",
+                            border: "1px solid black",
+                            bg: "white"
+                        }}
+                    />
+                    <Flex
+                        left="1px"
+                        right={0.1}
+                        bottom="1px"
+                        justify="flex-end"
+                        bg="gray.100"
+                        p="6px 8px"
+                        borderRadius="0px 0px 4px 4px"
+                    >
+                        <Button
+                            height="26px"
+                            disabled={!answerText.length}
+                            isLoading={createLoading}
+                            onClick={() => handleCreateAnswerReply(answerText)}
                         >
-                            <Button
-                                height="26px"
-                                disabled={!answerText.length}
-                                isLoading={createLoading}
-                                onClick={() => handleCreateAnswerReply(answerText)}
-                            >
-                                Reply
-                            </Button>
+                            Reply
+                        </Button>
+                    </Flex>
+                </>
+            ) : (
+                    <Flex
+                        align="center"
+                        justify="space-between"
+                        borderRadius={2}
+                        border="1px solid"
+                        borderColor="gray.100"
+                        p={4}
+                    >
+                        <Text fontWeight={600}>Log in or Sign Up to Answer this Question</Text>
+                        <Flex align="center">
+                            <AuthButtons />
                         </Flex>
-                    </>
-                ) : (
-                        <Flex
-                            align="center"
-                            justify="space-between"
-                            borderRadius={2}
-                            border="1px solid"
-                            borderColor="gray.100"
-                            p={4}
-                        >
-                            <Text fontWeight={600}>Log in or Sign Up to Answer this Question</Text>
-                            <Flex align="center">
-                                <AuthButtons />
-                            </Flex>
-                        </Flex>
-                    )}
-            </Flex>
-        </>
+                    </Flex>
+                )}
+        </Flex>
     );
 };
 
