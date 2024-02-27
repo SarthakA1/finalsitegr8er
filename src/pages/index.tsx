56a675b73ef5d545eb47328e417cff98b30371ee
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-02-27 14:43:02 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -344,7 +344,7 @@ const Home: NextPage = () => {
                   <span className={`filter_main_criteria_sub_section_without_backgrouund_border ${activeFilters.criteria && (activeFilters.criteria as string[]).includes('Criteria D') ? 'active' : ''}`} onClick={() => handleChangeTopFilter('criteria', 'Criteria D')}>Criteria D</span>
               </div>
               <div className='filter_main_difficulty_section'>
-                  <Text style={{fontSize: "12px", fontWeight: "600"}}>Difficulty(Academic Questions)</Text>
+                  <Text style={{fontSize: "12px", fontWeight: "600"}}>Difficulty (Academic Questions)</Text>
                   <span className={`filter_main_difficulty_sub_section ${activeFilters.difficulty && (activeFilters.difficulty as string[]).includes('easy') ? 'active' : ''}`} onClick={() => handleChangeTopFilter('difficulty', 'easy')}>Easy</span>
                   <span className={`filter_main_difficulty_sub_section ${activeFilters.difficulty && (activeFilters.difficulty as string[]).includes('medium') ? 'active' : ''}`} onClick={() => handleChangeTopFilter('difficulty', 'medium')}>Medium</span>
                   <span className={`filter_main_difficulty_sub_section ${activeFilters.difficulty && (activeFilters.difficulty as string[]).includes('hard') ? 'active' : ''}`} onClick={() => handleChangeTopFilter('difficulty', 'hard')}>Hard</span>
