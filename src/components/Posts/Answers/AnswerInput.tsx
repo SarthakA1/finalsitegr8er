6be7fc22e5c4221d22eb3960570ca4ef915073b1
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-03-01 13:35:29 +0530

    Update AnswerInput.tsx

--- src/components/Posts/Answers/AnswerInput.tsx
+++ src/components/Posts/Answers/AnswerInput.tsx
@@ -1,7 +1,9 @@
 import AuthButtons from '@/components/Navbar/RightContent/AuthButtons';
-import { Flex, Textarea, Button, Text } from '@chakra-ui/react';
+import { Flex, Button, Text } from '@chakra-ui/react';
 import { User } from 'firebase/auth';
 import React from 'react';
+import { Editor } from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; // Import Quill styles
 
 type AnswerInputProps = {
     answerText: string;
@@ -11,78 +13,77 @@ type AnswerInputProps = {
     onCreateAnswer: (answerText: string) => void;
 };
 
-const AnswerInput:React.FC<AnswerInputProps> = ({ answerText, setAnswerText, user, createLoading, onCreateAnswer}) => {
-    
+const AnswerInput: React.FC<AnswerInputProps> = ({ answerText, setAnswerText, user, createLoading, onCreateAnswer }) => {
+
     return (
-        <Flex direction="column" position="relative" >
-        {user ? (
-          <>
-            
-            <Text mb={1}>
-              Answer as{" "}
-              <span style={{ color: "#2c75b9" }}>
-              {user?.displayName! || user?.email!.split("@")[0]}
-              </span>
-            </Text>
-            
-            <Textarea
-              value={answerText}
-              onChange={(event) => setAnswerText(event.target.value)}
-                placeholder="What is the Answer to this Question?"
-              fontSize="10pt"
-              borderRadius={4}
-              minHeight="160px"
-              pb={10}
-              _placeholder={{ color: "gray.500" }}
-              _focus={{
-                outline: "none",
-                border: "1px solid black",
-                bg:"white"
-                
-              }}
-            />
-            <Flex
-             
-             left="1px"
-             right={0.1}
-            
-             bottom="1px"
-             justify="flex-end"
-             bg="gray.100"
-             p="6px 8px"
-             borderRadius="0px 0px 4px 4px"
-           >
-             <Button 
-               height="26px"
-               disabled={!answerText.length}
-               isLoading={createLoading}
-               onClick={() => onCreateAnswer(answerText)}
-             >
-               Answer
-             </Button>
-           </Flex>
-            
-            
-          </>
-          
-        ) : (
-          <Flex
-            align="center"
-            justify="space-between"
-            borderRadius={2}
-            border="1px solid"
-            borderColor="gray.100"
-            p={4}
-          >
-            <Text fontWeight={600}>Log in or Sign Up to Answer this Question</Text>
-            <Flex align="center">
-            <AuthButtons />
-            </Flex>
-          </Flex>
-        )}
-      </Flex>
-      
-        )
-        
+        <Flex direction="column" position="relative">
+            {user ? (
+                <>
+
+                    <Text mb={1}>
+                        Answer as{" "}
+                        <span style={{ color: "#2c75b9" }}>
+                            {user?.displayName! || user?.email!.split("@")[0]}
+                        </span>
+                    </Text>
+
+                    <Editor
+                        value={answerText}
+                        onChange={setAnswerText}
+                        placeholder="What is the Answer to this Question?"
+                        theme="snow" // Use snow theme
+                        modules={{
+                            toolbar: [
+                                [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
+                                [{ size: [] }],
+                                ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+                                [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+                                ['link', 'image'],
+                                ['clean']
+                            ],
+                        }}
+                    />
+                    <Flex
+                        left="1px"
+                        right={0.1}
+
+                        bottom="1px"
+                        justify="flex-end"
+                        bg="gray.100"
+                        p="6px 8px"
+                        borderRadius="0px 0px 4px 4px"
+                    >
+                        <Button
+                            height="26px"
+                            disabled={!answerText.length}
+                            isLoading={createLoading}
+                            onClick={() => onCreateAnswer(answerText)}
+                        >
+                            Answer
+                        </Button>
+                    </Flex>
+
+
+                </>
+
+            ) : (
+                <Flex
+                    align="center"
+                    justify="space-between"
+                    borderRadius={2}
+                    border="1px solid"
+                    borderColor="gray.100"
+                    p={4}
+                >
+                    <Text fontWeight={600}>Log in or Sign Up to Answer this Question</Text>
+                    <Flex align="center">
+                        <AuthButtons />
+                    </Flex>
+                </Flex>
+            )}
+        </Flex>
+
+    )
+
 }
 export default AnswerInput;
