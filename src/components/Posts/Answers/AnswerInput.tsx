558d45656a499c7eaf2c3835108f8e2312f01caf
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-03-03 02:33:38 +0530

    Update AnswerInput.tsx

--- src/components/Posts/Answers/AnswerInput.tsx
+++ src/components/Posts/Answers/AnswerInput.tsx
@@ -1,7 +1,8 @@
 import AuthButtons from '@/components/Navbar/RightContent/AuthButtons';
-import { Flex, Textarea, Button, Text } from '@chakra-ui/react';
+import { Flex, Button, Text } from '@chakra-ui/react';
 import { User } from 'firebase/auth';
-import React from 'react';
+import React, { useState } from 'react';
+import { Editor } from '../../common/Editor'; // Import the Editor component
 
 type AnswerInputProps = {
     answerText: string;
@@ -11,13 +12,20 @@ type AnswerInputProps = {
     onCreateAnswer: (answerText: string) => void;
 };
 
-const AnswerInput:React.FC<AnswerInputProps> = ({ answerText, setAnswerText, user, createLoading, onCreateAnswer}) => {
+const AnswerInput: React.FC<AnswerInputProps> = ({ answerText, setAnswerText, user, createLoading, onCreateAnswer}) => {
+    // State to manage rich text content in the editor
+    const [editorText, setEditorText] = useState<string>(answerText);
+
+    // Function to handle editor content change
+    const handleEditorChange = (content: string) => {
+        setEditorText(content);
+        setAnswerText(content); // Synchronize editor content with answerText state
+    };
     
     return (
-        <Flex direction="column" position="relative" >
+        <Flex direction="column" position="relative">
         {user ? (
           <>
-            
             <Text mb={1}>
               Answer as{" "}
               <span style={{ color: "#2c75b9" }}>
@@ -25,44 +33,31 @@ const AnswerInput:React.FC<AnswerInputProps> = ({ answerText, setAnswerText, use
               </span>
             </Text>
             
-            <Textarea
-              value={answerText}
-              onChange={(event) => setAnswerText(event.target.value)}
-                placeholder="What is the Answer to this Question?"
-              fontSize="10pt"
-              borderRadius={4}
-              minHeight="160px"
-              pb={10}
-              _placeholder={{ color: "gray.500" }}
-              _focus={{
-                outline: "none",
-                border: "1px solid black",
-                bg:"white"
-                
-              }}
+            {/* Replace Textarea with Editor */}
+            <Editor
+              value={editorText}
+              onChange={handleEditorChange}
+              placeholder="What is the Answer to this Question?"
             />
+            
             <Flex
-             
-             left="1px"
-             right={0.1}
-            
-             bottom="1px"
-             justify="flex-end"
-             bg="gray.100"
-             p="6px 8px"
-             borderRadius="0px 0px 4px 4px"
-           >
-             <Button 
-               height="26px"
-               disabled={!answerText.length}
-               isLoading={createLoading}
-               onClick={() => onCreateAnswer(answerText)}
-             >
-               Answer
-             </Button>
-           </Flex>
-            
-            
+              left="1px"
+              right={0.1}
+              bottom="1px"
+              justify="flex-end"
+              bg="gray.100"
+              p="6px 8px"
+              borderRadius="0px 0px 4px 4px"
+            >
+              <Button 
+                height="26px"
+                disabled={!editorText.length}
+                isLoading={createLoading}
+                onClick={() => onCreateAnswer(editorText)}
+              >
+                Answer
+              </Button>
+            </Flex>
           </>
           
         ) : (
@@ -76,13 +71,12 @@ const AnswerInput:React.FC<AnswerInputProps> = ({ answerText, setAnswerText, use
           >
             <Text fontWeight={600}>Log in or Sign Up to Answer this Question</Text>
             <Flex align="center">
-            <AuthButtons />
+              <AuthButtons />
             </Flex>
           </Flex>
         )}
       </Flex>
-      
-        )
-        
-}
+    );
+};
+
 export default AnswerInput;
