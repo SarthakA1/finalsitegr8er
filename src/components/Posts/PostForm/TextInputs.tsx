4748da652c9105f752747828a4b8bcd2854425f4
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-03-03 02:18:39 +0530

    Update TextInputs.tsx

--- src/components/Posts/PostForm/TextInputs.tsx
+++ src/components/Posts/PostForm/TextInputs.tsx
@@ -54,41 +54,103 @@ const TextInputs: React.FC<TextInputsProps> = ({
       },
     } as React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>);
   };
-  const [gradeError, setGradeError] = useState<string | null>(null);
   const [typeOfQuestionsError, setTypeOfQuestionsError] = useState<string | null>(
     null
   );
-  const [titleError, setTitleError] = useState<string | null>(null);
-
-  // Custom validation function for MYP grade options
-  const validateGrade = (value: any) => {
-    let error = null;
-    if (!value.grade || !value.grade.value) {
-      error = "MYP Grade is required";
-    }
-    setGradeError(error);
-  };
 
   // Custom validation function for "Type Of Questions" field
   const validateTypeOfQuestions = (value: any) => {
     let error = null;
-    if (!value.typeOfQuestions || !value.typeOfQuestions.value) {
+    if (!value.typeOfQuestions) {
       error = "Type Of Questions is required";
     }
     setTypeOfQuestionsError(error);
-  };
-
-  // Custom validation function for title
-  const validateTitle = (value: any) => {
-    let error = null;
-    if (!value.title) {
-      error = "Title is required";
+    if (error == null) {
+      handleCreatePost();
     }
-    setTitleError(error);
   };
 
+  const criteriaOptions = [
+    {
+      value: "Criteria A",
+      label: "Criteria A",
+    },
+    {
+      value: "Criteria B",
+      label: "Criteria B",
+    },
+    {
+      value: "Criteria C",
+      label: "Criteria C",
+    },
+    {
+      value: "Criteria D",
+      label: "Criteria D",
+    },
+  ];
+  const typeOfQuestionsOptions = [
+    {
+      value: "Academic Question",
+      label: "Academic Question",
+      tooltip: "Academic questions are those that have a specific answer and usually come from school or test materials like worksheets, exams, or past papers. They often involve solving problems or answering questions with a single correct solution, such as math problems or exam-style questions.",
+    },
+    {
+      value: "General Doubt",
+      label: "General Doubt",
+      tooltip: "General doubts are open-ended and can cover a wide range of topics. They might include asking for advice, requesting resources, seeking tips or tricks, or any other generic queries you may want an answer to",
+    },
+  ];
+  const gradeOptions = [
+    {
+      value: "1",
+      label: "MYP 1",
+    },
+    {
+      value: "2",
+      label: "MYP 2",
+    },
+    {
+      value: "3",
+      label: "MYP 3",
+    },
+    {
+      value: "4",
+      label: "MYP 4",
+    },
+    {
+      value: "5",
+      label: "MYP 5",
+    },
+  ];
+  const customCriteriaComponents = {
+    Option: ({ children, ...props }: any) => (
+      <chakraComponents.Option {...props}>{children}</chakraComponents.Option>
+    ),
+  };
+  const customTypeOfQuestionsComponents = {
+    Option: ({ children, ...props }: any) => (
+      <chakraComponents.Option {...props}>
+        <Flex alignItems="center">
+          <Text>{children}</Text>
+          {props.data.tooltip && (
+            <Tooltip label={props.data.tooltip}>
+              <Icon as={InfoOutlineIcon} color="gray.500" ml={2} />
+            </Tooltip>
+          )}
+        </Flex>
+      </chakraComponents.Option>
+    ),
+  };
+  const customGradeComponents = {
+    Option: ({ children, ...props }: any) => (
+      <chakraComponents.Option {...props}>{children}</chakraComponents.Option>
+    ),
+  };
   return (
     <Stack spacing={3} width="100%">
+{/*       <Text style={{ marginLeft: 0.5, marginTop: 2, fontWeight: 500 }}>
+        MYP
+      </Text> */}
       <SimpleGrid columns={2} spacing={10}>
         <Flex direction="row" style={{ display: "block" }}>
           <Select
@@ -96,22 +158,11 @@ const TextInputs: React.FC<TextInputsProps> = ({
             placeholder="MYP"
             components={customGradeComponents}
             value={textInputs.grade.value && textInputs.grade} // Set the value prop for controlled component
-            onChange={(selectedGradeOptions: any) => {
-              handleInputChange("grade", selectedGradeOptions);
-              validateGrade({ grade: selectedGradeOptions });
-            }}
+            onChange={(selectedGradeOptions: any) =>
+              handleInputChange("grade", selectedGradeOptions)
+            }
             options={gradeOptions}
           />
-          <p
-            style={{
-              color: "#ff0000",
-              fontSize: "12px",
-              paddingLeft: "3px",
-              paddingTop: "3px",
-            }}
-          >
-            {gradeError}
-          </p>
         </Flex>
         <Flex style={{ display: "block" }}>
           {textInputs.criteria.value && (
@@ -140,10 +191,9 @@ const TextInputs: React.FC<TextInputsProps> = ({
             placeholder="Type Of Question"
             components={customTypeOfQuestionsComponents}
             value={textInputs.typeOfQuestions.value && textInputs.typeOfQuestions} // Set the value prop for controlled component
-            onChange={(selectedtypeOfQuestionsOptions: any) => {
-              handleInputChange("typeOfQuestions", selectedtypeOfQuestionsOptions);
-              validateTypeOfQuestions({ typeOfQuestions: selectedtypeOfQuestionsOptions });
-            }}
+            onChange={(selectedtypeOfQuestionsOptions: any) =>
+              handleInputChange("typeOfQuestions", selectedtypeOfQuestionsOptions)
+            }
             options={typeOfQuestionsOptions}
           />
           <p
@@ -160,10 +210,7 @@ const TextInputs: React.FC<TextInputsProps> = ({
         <Input
           name="title"
           value={textInputs.title}
-          onChange={(e: any) => {
-            handleInputChange("title", e.target.value);
-            validateTitle({ title: e.target.value });
-          }}
+          onChange={(e: any) => handleInputChange("title", e.target.value)}
           _placeholder={{ color: "gray.500" }}
           _focus={{
             outline: "none",
@@ -175,16 +222,6 @@ const TextInputs: React.FC<TextInputsProps> = ({
           borderRadius={4}
           placeholder="Topic"
         />
-        <p
-          style={{
-            color: "#ff0000",
-            fontSize: "12px",
-            paddingLeft: "3px",
-            paddingTop: "3px",
-          }}
-        >
-          {titleError}
-        </p>
       </SimpleGrid>
       <Editor
         id="body"
@@ -198,16 +235,9 @@ const TextInputs: React.FC<TextInputsProps> = ({
         <Button
           height="34px"
           padding="0px 30px"
-          disabled={!textInputs.title || !textInputs.body || !textInputs.typeOfQuestions || !textInputs.typeOfQuestions.value || !textInputs.grade || !textInputs.grade.value || titleError || typeOfQuestionsError || gradeError}
+          disabled={!textInputs.title || !textInputs.body}
           isLoading={loading}
-          onClick={() => {
-            validateTypeOfQuestions(textInputs);
-            validateGrade(textInputs);
-            validateTitle(textInputs);
-            if (!titleError && !typeOfQuestionsError && !gradeError) {
-              handleCreatePost();
-            }
-          }}
+          onClick={() => validateTypeOfQuestions(textInputs)}
         >
           Ask
         </Button>
