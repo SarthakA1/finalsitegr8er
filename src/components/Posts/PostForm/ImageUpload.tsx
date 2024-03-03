ee13f7c48e2c2e43b02cecbe2223d8fb8c3b8848
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-03-03 12:33:39 +0530

    Update ImageUpload.tsx

--- src/components/Posts/PostForm/ImageUpload.tsx
+++ src/components/Posts/PostForm/ImageUpload.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Flex, Button, Image, Stack } from "@chakra-ui/react";
+import React, { Ref } from "react";
+import { Flex, Stack, Button, Image, Grid, GridItem } from "@chakra-ui/react";
 
 type ImageUploadProps = {
   selectedFile: string[];
@@ -18,89 +18,83 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
 }) => {
   return (
     <Flex direction="column" justify="center" align="center" width="100%">
-      <Flex
-        justify="center"
-        align="center"
-        p={20}
-        border="1px dashed"
-        borderColor="gray.200"
-        borderRadius={4}
-        width="100%"
-      >
-        <Button
-          variant="outline"
-          height="28px"
-          onClick={() => selectFileRef.current?.click()}
-        >
-          Upload
-        </Button>
-        <input
-          id="file-upload"
-          type="file"
-          accept="image/png,image/gif,image/jpeg, application/pdf,application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-          hidden
-          ref={selectFileRef}
-          onChange={onSelectImage}
-          multiple
-        />
-      </Flex>
-      {selectedFile.length > 0 &&
-        selectedFile.map((file, index) => {
-          const matchResult = file.match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/);
-          let mimeType;
+      <ul style={{listStyle: 'none', display: 'flex'}}>
+      {selectedFile.length > 0 
+        ?
+          selectedFile?.map(file => {
+            const matchResult = file.match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/);
+            let mimeType;
 
-          if (matchResult && matchResult.length > 0) {
-            mimeType = matchResult[0];
-          } else {
-            // Handle case when no match is found
-            // You may assign a default mimeType or throw an error
-          }
-          let fileImage = "";
-          if (mimeType === "application/pdf") {
-            fileImage = "/images/pdf.png";
-          } else if (
-            mimeType === "image/png" ||
-            mimeType === "image/jpeg"
-          ) {
-            fileImage = file;
-          } else if (
-            mimeType === "application/msword" ||
-            mimeType ===
-              "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-          ) {
-            fileImage = "/images/docs.png";
-          }
-          return (
-            <Flex
-              key={index}
-              justify="center"
-              align="center"
-              direction="column"
-              mt={4}
+            if (matchResult && matchResult.length > 0) {
+                mimeType = matchResult[0];
+            } else {
+                // Handle case when no match is found
+                // You may assign a default mimeType or throw an error
+            }
+            //let mimeType = file.match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/)[0];
+            let fileImage = '';
+            if(mimeType === 'application/pdf'){
+              fileImage = '/images/pdf.png';
+            } else if (mimeType === 'image/png' || mimeType === 'image/jpeg'){
+              fileImage = file;
+            } else if(mimeType === 'application/msword' || mimeType === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'){
+              fileImage = '/images/docs.png'
+            }
+            return(
+              <>
+                  
+                    <li style={{listStyle: 'none', border: '1px solid #ccc', borderRadius: '10px', margin: "5px"}}>
+                      <Image
+                        src={fileImage}
+                        maxWidth="400px"
+                        maxHeight="400px"
+                        style={{padding: "10px", width: "100%"}}
+                      />
+                      <Stack direction="row" mt={4} style={{textAlign: "center", display: "block"}}>
+                        <Button
+                          variant="outline"
+                          height="28px"
+                          onClick={() => setSelectedFile([])}
+                        >
+                          Remove
+                        </Button>
+                      </Stack>
+                    </li>
+              </>
+            )
+          })
+        :
+          <Flex
+            justify="center"
+            align="center"
+            p={20}
+            border="1px dashed"
+            borderColor="gray.200"
+            borderRadius={4}
+            width="100%"
+          >
+           
+            <input
+              id="file-upload"
+              type="file"
+              accept="image/png,image/gif,image/jpeg, application/pdf,application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+              hidden
+              ref={selectFileRef}
+              onChange={onSelectImage}
+              multiple
+            />
+          </Flex>
+      }
+      </ul>
+      <Button
+              variant="outline"
+              height="28px"
+              onClick={() => selectFileRef.current?.click()}
             >
-              <Image
-                src={fileImage}
-                maxWidth="400px"
-                maxHeight="400px"
-                style={{ padding: "10px", width: "100%" }}
-              />
-              <Stack direction="row" mt={4} style={{ textAlign: "center" }}>
-                <Button
-                  variant="outline"
-                  height="28px"
-                  onClick={() =>
-                    setSelectedFile((prevFiles) =>
-                      prevFiles.filter((_, i) => i !== index)
-                    )
-                  }
-                >
-                  Remove
-                </Button>
-              </Stack>
-            </Flex>
-          );
-        })}
+              Upload
+            </Button>
     </Flex>
+    
   );
 };
 export default ImageUpload;
