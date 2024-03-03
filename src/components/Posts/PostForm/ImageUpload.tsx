8fa54e8ace33a57d40f5d02527e7cda1a4dda659
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-03-03 12:37:10 +0530

    Update ImageUpload.tsx

--- src/components/Posts/PostForm/ImageUpload.tsx
+++ src/components/Posts/PostForm/ImageUpload.tsx
@@ -1,10 +1,9 @@
-import React, { Ref } from "react";
-import { Flex, Stack, Button, Image, Grid, GridItem } from "@chakra-ui/react";
+import React, { useRef } from "react";
+import { Flex, Button, Image } from "@chakra-ui/react";
 
 type ImageUploadProps = {
   selectedFile: string[];
   setSelectedFile: (value: string[]) => void;
-  setSelectedTab: (value: string) => void;
   selectFileRef: React.RefObject<HTMLInputElement>;
   onSelectImage: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
@@ -12,58 +11,67 @@ type ImageUploadProps = {
 const ImageUpload: React.FC<ImageUploadProps> = ({
   selectedFile,
   setSelectedFile,
-  setSelectedTab,
   selectFileRef,
   onSelectImage,
 }) => {
   return (
     <Flex direction="column" justify="center" align="center" width="100%">
-      <ul style={{listStyle: 'none', display: 'flex'}}>
-      {selectedFile.length > 0 
-        ?
-          selectedFile?.map(file => {
+      <ul style={{ listStyle: "none", display: "flex" }}>
+        {selectedFile.length > 0 ? (
+          selectedFile.map((file, index) => {
             const matchResult = file.match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/);
-            let mimeType;
+            let mimeType = "";
 
             if (matchResult && matchResult.length > 0) {
-                mimeType = matchResult[0];
+              mimeType = matchResult[0];
             } else {
-                // Handle case when no match is found
-                // You may assign a default mimeType or throw an error
+              // Handle case when no match is found
+              // You may assign a default mimeType or throw an error
             }
-            //let mimeType = file.match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/)[0];
-            let fileImage = '';
-            if(mimeType === 'application/pdf'){
-              fileImage = '/images/pdf.png';
-            } else if (mimeType === 'image/png' || mimeType === 'image/jpeg'){
+            let fileImage = "";
+            if (mimeType === "application/pdf") {
+              fileImage = "/images/pdf.png";
+            } else if (
+              mimeType === "image/png" ||
+              mimeType === "image/jpeg"
+            ) {
               fileImage = file;
-            } else if(mimeType === 'application/msword' || mimeType === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'){
-              fileImage = '/images/docs.png'
+            } else if (
+              mimeType === "application/msword" ||
+              mimeType ===
+                "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+            ) {
+              fileImage = "/images/docs.png";
             }
-            return(
-              <>
-                  
-                    <li style={{listStyle: 'none', border: '1px solid #ccc', borderRadius: '10px', margin: "5px"}}>
-                      <Image
-                        src={fileImage}
-                        maxWidth="400px"
-                        maxHeight="400px"
-                        style={{padding: "10px", width: "100%"}}
-                      />
-                      <Stack direction="row" mt={4} style={{textAlign: "center", display: "block"}}>
-                        <Button
-                          variant="outline"
-                          height="28px"
-                          onClick={() => setSelectedFile([])}
-                        >
-                          Remove
-                        </Button>
-                      </Stack>
-                    </li>
-              </>
-            )
+            return (
+              <li
+                key={index}
+                style={{
+                  listStyle: "none",
+                  border: "1px solid #ccc",
+                  borderRadius: "10px",
+                  margin: "5px",
+                }}
+              >
+                <Image
+                  src={fileImage}
+                  maxWidth="400px"
+                  maxHeight="400px"
+                  style={{ padding: "10px", width: "100%" }}
+                />
+                <Button
+                  variant="outline"
+                  height="28px"
+                  onClick={() =>
+                    setSelectedFile(selectedFile.filter((_, i) => i !== index))
+                  }
+                >
+                  Remove
+                </Button>
+              </li>
+            );
           })
-        :
+        ) : (
           <Flex
             justify="center"
             align="center"
@@ -73,7 +81,6 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
             borderRadius={4}
             width="100%"
           >
-           
             <input
               id="file-upload"
               type="file"
@@ -84,17 +91,16 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
               multiple
             />
           </Flex>
-      }
+        )}
       </ul>
       <Button
-              variant="outline"
-              height="28px"
-              onClick={() => selectFileRef.current?.click()}
-            >
-              Upload
-            </Button>
+        variant="outline"
+        height="28px"
+        onClick={() => selectFileRef.current?.click()}
+      >
+        Upload
+      </Button>
     </Flex>
-    
   );
 };
 export default ImageUpload;
