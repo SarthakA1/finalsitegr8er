61a50f4132d79eec16d035a1fa07c02795d52aa6
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-03-04 18:46:46 +0530

    pics codeofhonor

--- src/pages/codeofhonor.tsx
+++ src/pages/codeofhonor.tsx
@@ -16,7 +16,7 @@ const codeofhonor:React.FC<codeofhonorProps> = () => {
                         <h2 className="rule-heading blue-heading">Respect Copyright Laws and Promote Academic Integrity</h2>
                         <p className="rule-text">Users must refrain from posting any copyrighted material, including MYP past papers and content copyrighted by the International Baccalaureate or other organizations, to uphold the integrity of original work. Users are also always expected to provide authentic and original responses avoiding plagiarism.</p>
                     </div>
-                    <img src="image1.jpg" alt="Respect Copyright Laws and Promote Academic Integrity" className="rule-image" />
+                    <img src="public/images/copyrightlaws.png" alt="Respect Copyright Laws and Promote Academic Integrity" className="rule-image" />
                 </div>
                 {/* Second Rule */}
                 <div className="code-of-honor-rule">
@@ -24,7 +24,7 @@ const codeofhonor:React.FC<codeofhonorProps> = () => {
                         <h2 className="rule-heading green-heading">Foster Collaborative Learning</h2>
                         <p className="rule-text">Encourage sharing knowledge and experiences responsibly while ensuring all users feel a sense of belonging. Avoid any racial slurs or political dialogue.</p>
                     </div>
-                    <img src="image2.jpg" alt="Foster Collaborative Learning" className="rule-image" />
+                    <img src="public/images/collaboration.png" alt="Foster Collaborative Learning" className="rule-image" />
                 </div>
             </div>
             {/* Bottom Row */}
@@ -35,7 +35,7 @@ const codeofhonor:React.FC<codeofhonorProps> = () => {
                         <h2 className="rule-heading orange-heading">Report Violations Promptly</h2>
                         <p className="rule-text">Users have a responsibility to promptly report any instances of copyright infringement or academic dishonesty to maintain the integrity of the platform.</p>
                     </div>
-                    <img src="image3.jpg" alt="Report Violations Promptly" className="rule-image" />
+                    <img src="public/images/report.png" alt="Report Violations Promptly" className="rule-image" />
                 </div>
                 {/* Fourth Rule */}
                 <div className="code-of-honor-rule">
@@ -43,7 +43,7 @@ const codeofhonor:React.FC<codeofhonorProps> = () => {
                         <h2 className="rule-heading red-heading">Uphold Community Accountability</h2>
                         <p className="rule-text">By adhering to this code of honor, users contribute to the creation of a supportive and ethical learning environment, fostering trust and credibility within the GR8ER community.</p>
                     </div>
-                    <img src="image4.jpg" alt="Uphold Community Accountability" className="rule-image" />
+                    <img src="public/images/accountability.png" alt="Uphold Community Accountability" className="rule-image" />
                 </div>
             </div>
         </div>
