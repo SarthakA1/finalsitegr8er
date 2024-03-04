95185c11e781e755aba9dd8bff52c5c2f4cdab5b
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-03-04 21:47:12 +0530

    changes coh

--- src/pages/codeofhonor.tsx
+++ src/pages/codeofhonor.tsx
@@ -6,46 +6,48 @@ type codeofhonorProps = {
 
 const codeofhonor:React.FC<codeofhonorProps> = () => {
     
-    return  (
-        <div className="code-of-honor-container">
-            {/* Top Row */}
-            <div className="code-of-honor-row">
-                {/* First Rule */}
-                <div className="code-of-honor-rule">
-                    <div className="rule-content">
-                        <h2 className="rule-heading blue-heading">Respect Copyright Laws and Promote Academic Integrity</h2>
-                        <p className="rule-text">Users must refrain from posting any copyrighted material, including MYP past papers and content copyrighted by the International Baccalaureate or other organizations, to uphold the integrity of original work. Users are also always expected to provide authentic and original responses avoiding plagiarism.</p>
-                    </div>
-                    <img src="/images/copyrightlaws.png" className="rule-image" />
-                </div>
-                {/* Second Rule */}
-                <div className="code-of-honor-rule">
-                    <div className="rule-content">
-                        <h2 className="rule-heading green-heading">Foster Collaborative Learning</h2>
-                        <p className="rule-text">Encourage sharing knowledge and experiences responsibly while ensuring all users feel a sense of belonging. Avoid any racial slurs or political dialogue.</p>
-                    </div>
-                    <img src="/images/collaboration.png"className="rule-image" />
-                </div>
+    return   (
+
+      <div className="code-of-honor-container">
+    {/* Top Row */}
+    <div className="code-of-honor-row">
+        {/* First Rule */}
+        <div className="code-of-honor-rule">
+            <div className="rule-content">
+                <h2 className="rule-heading blue-heading">Respect Copyright Laws and Promote Academic Integrity</h2>
+                <p className="rule-text">Users must refrain from posting any copyrighted material, including MYP past papers and content copyrighted by the International Baccalaureate or other organizations, to uphold the integrity of original work. Users are also always expected to provide authentic and original responses avoiding plagiarism.</p>
             </div>
-            {/* Bottom Row */}
-            <div className="code-of-honor-row">
-                {/* Third Rule */}
-                <div className="code-of-honor-rule">
-                    <div className="rule-content">
-                        <h2 className="rule-heading orange-heading">Report Violations Promptly</h2>
-                        <p className="rule-text">Users have a responsibility to promptly report any instances of copyright infringement or academic dishonesty to maintain the integrity of the platform.</p>
-                    </div>
-                    <img src="/images/report.png" className="rule-image" />
-                </div>
-                {/* Fourth Rule */}
-                <div className="code-of-honor-rule">
-                    <div className="rule-content">
-                        <h2 className="rule-heading red-heading">Uphold Community Accountability</h2>
-                        <p className="rule-text">By adhering to this code of honor, users contribute to the creation of a supportive and ethical learning environment, fostering trust and credibility within the GR8ER community.</p>
-                    </div>
-                    <img src="/images/accountability.png" className="rule-image" />
-                </div>
-            </div>
+            <img src="/images/copyrightlaws.png" className="rule-image" alt="Copyright Laws" />
         </div>
-    );}
+        {/* Second Rule */}
+        <div className="code-of-honor-rule">
+            <div className="rule-content">
+                <h2 className="rule-heading green-heading">Foster Collaborative Learning</h2>
+                <p className="rule-text">Encourage sharing knowledge and experiences responsibly while ensuring all users feel a sense of belonging. Avoid any racial slurs or political dialogue.</p>
+            </div>
+            <img src="/images/collaboration.png"className="rule-image" alt="Collaboration" />
+        </div>
+    </div>
+    {/* Bottom Row */}
+    <div className="code-of-honor-row">
+        {/* Third Rule */}
+        <div className="code-of-honor-rule">
+            <div className="rule-content">
+                <h2 className="rule-heading orange-heading">Report Violations Promptly</h2>
+                <p className="rule-text">Users have a responsibility to promptly report any instances of copyright infringement or academic dishonesty to maintain the integrity of the platform.</p>
+            </div>
+            <img src="/images/report.png" className="rule-image" alt="Report Violations" />
+        </div>
+        {/* Fourth Rule */}
+        <div className="code-of-honor-rule">
+            <div className="rule-content">
+                <h2 className="rule-heading red-heading">Uphold Community Accountability</h2>
+                <p className="rule-text">By adhering to this code of honor, users contribute to the creation of a supportive and ethical learning environment, fostering trust and credibility within the GR8ER community.</p>
+            </div>
+            <img src="/images/accountability.png" className="rule-image" alt="Community Accountability" />
+        </div>
+    </div>
+</div>
+);
+}
 export default codeofhonor;
