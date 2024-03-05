336a0facd56ebce277c080df6b1a0c6c686d3028
devbinary003 <binarydata.sale@gmail.com>
2024-03-05 18:42:51 +0530

    fixed formulla duplicate text show issue code changes upload
    fixed formulla duplicate text show issue code changes upload

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex/dist/katex.min.css"/>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/markdown-it-texmath/css/texmath.min.css"/>
       <body>
         <Main />
         <NextScript />
