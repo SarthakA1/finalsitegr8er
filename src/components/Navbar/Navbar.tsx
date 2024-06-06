106a50552c843b95250a89f6895618c798eb3bf8
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-06-07 00:31:40 +0530

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,13 +8,13 @@ import Directory from './Directory/Directory';
 import useDirectory from '@/hooks/useDirectory';
 import { defaultMenuItem } from '@/atoms/directoryMenuAtom';
 
-const Navbar: React.FC = () => {
+const navbar: React.FC = () => {
     const [user, loading, error] = useAuthState(auth);
     const { onSelectMenuItem } = useDirectory();
 
     return (
-        <Flex bg='white' height='50px' padding='6px 10px' width="100%" align="center" justify="space-between">
-            <Flex align="center" cursor="pointer" onClick={() => onSelectMenuItem(defaultMenuItem)}>
+        <Flex bg='white' height='50px' padding='6px 10px' direction="row">
+            <Flex align="center" cursor="pointer" onClick={() => onSelectMenuItem(defaultMenuItem)} >
                 <a href="/">
                     <Image src="/images/gr8er.png" ml={.3} height="45px" display={{ base: 'unset', md: 'none' }} />
                     <Image src="/images/gr8er logo.png" ml={2} mt={2} height="37px" mb="1px" display={{ base: 'none', md: 'unset' }} />
@@ -24,27 +24,29 @@ const Navbar: React.FC = () => {
             {user && (
                 <Flex align="center">
                     <Directory />
+                    
                 </Flex>
             )}
             
-            <Box display={{ base: 'none', sm: 'flex' }} flex="1" justifyContent="center">
+           
                 <Searchinput />
-            </Box>
+
 
             <Button 
-                height="35px"
-                width="150px"
-                onClick={() => window.location.href = 'https://www.gr8er.live/'}
-                mr={3}
-                mt={0.5}>
-                Coming soon!
-            </Button>
+        height="35px"
+        width= "150px"
+        onClick={() => window.location.href = 'https://www.gr8er.live/'}
+        mr={3} 
+        mt={0.5}>
+         Coming soon!
+         </Button>
+           
 
-            <Flex>
+            <Flex >
                 <RightContent user={user} />
             </Flex>
         </Flex>
     );
 };
 
-export default Navbar;
+export default navbar;
