808e6582da0880d62db610b16255cb11a9930920
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-06-06 23:20:00 +0530

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Flex, Image } from '@chakra-ui/react';
+import { Flex, Image, Button } from '@chakra-ui/react';
 import Searchinput from './Searchinput';
 import RightContent from './RightContent/RightContent';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -8,30 +8,34 @@ import Directory from './Directory/Directory';
 import useDirectory from '@/hooks/useDirectory';
 import { defaultMenuItem } from '@/atoms/directoryMenuAtom';
 
-
-const navbar:React.FC = () => {
+const navbar: React.FC = () => {
     const [user, loading, error] = useAuthState(auth);
-    const {onSelectMenuItem}  = useDirectory()
+    const { onSelectMenuItem } = useDirectory();
 
     return (
-    <Flex bg='white' height='50px' padding='6px 10px' direction="row">
-        <Flex align="center" cursor="pointer" onClick={() => onSelectMenuItem(defaultMenuItem)} >
-            <a href="/">
-                <Image src="/images/gr8er.png" ml={.3} height="45px" display={{ base: 'unset', md: 'none' }}/>
-                <Image src="/images/gr8er logo.png" ml={2} mt={2} height="37px" mb="1px" display={{ base: 'none', md: 'unset' }}/>
-            </a>
-        </Flex>
-        
-        {user && <Directory/ >}
-        <Searchinput/>
-       
+        <Flex bg='white' height='50px' padding='6px 10px' direction="row">
+            <Flex align="center" cursor="pointer" onClick={() => onSelectMenuItem(defaultMenuItem)} >
+                <a href="/">
+                    <Image src="/images/gr8er.png" ml={.3} height="45px" display={{ base: 'unset', md: 'none' }} />
+                    <Image src="/images/gr8er logo.png" ml={2} mt={2} height="37px" mb="1px" display={{ base: 'none', md: 'unset' }} />
+                </a>
+            </Flex>
 
-        
-        <Flex >
-         <RightContent user={user} />
-         </Flex>
-         
-    </Flex>
-    )
+            {user && (
+                <Flex align="center">
+                    <Directory />
+                    <Button ml={4} onClick={() => window.location.href = 'https://your-link.com'}>
+                        Coming soon!
+                    </Button>
+                </Flex>
+            )}
+            <Searchinput />
+
+            <Flex >
+                <RightContent user={user} />
+            </Flex>
+        </Flex>
+    );
 };
+
 export default navbar;
