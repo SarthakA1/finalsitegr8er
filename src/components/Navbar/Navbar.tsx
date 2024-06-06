f19cb6ad2397d66d461cd182f1e8bffa687c77a3
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-06-07 00:27:25 +0530

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -26,16 +26,10 @@ const Navbar: React.FC = () => {
                     <Directory />
                 </Flex>
             )}
-
-            {user ? (
-                <Box display={{ base: 'flex', sm: 'flex' }} flex="1" justifyContent={{ base: 'flex-start', sm: 'center' }} maxWidth={{ base: '100px', sm: 'auto' }}>
-                    <Searchinput />
-                </Box>
-            ) : (
-                <Box display={{ base: 'none', sm: 'flex' }} flex="1" justifyContent="center">
-                    <Searchinput />
-                </Box>
-            )}
+            
+            <Box display={{ base: 'none', sm: 'flex' }} flex="1" justifyContent="center">
+                <Searchinput />
+            </Box>
 
             <Button 
                 height="35px"
