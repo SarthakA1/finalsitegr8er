a46b52f7a40f16e94ad486d7f1aee0e2706fca8f
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-06-28 14:30:32 +0530

    Update layout.tsx

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,31 +1,16 @@
 import React, { ReactNode } from "react";
 import Navbar from "../Navbar/Navbar";
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '@/firebase/clientApp';
+import Header from "../Subject/Header";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const [user, loading, error] = useAuthState(auth);
-
   return (
-    <>
-      {user ? (
-        <>
-          <Navbar />
-          <main>{children}</main>
-        </>
-      ) : (
-        <iframe
-          src="/landingpage.html" // Adjust the path based on your project structure
-          style={{ width: '100%', height: '100vh', border: 'none' }}
-          title="landing-page"
-        />
-      )}
-    </>
-  );
-};
-
+  <>
+    <Navbar />
+    <main>{children}</main>
+  </>
+)}
 export default Layout;
