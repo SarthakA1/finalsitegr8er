89f6e7fdecb2094e051909c30a01537a7e2918c6
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-06-28 14:16:43 +0530

    Update layout.tsx

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,16 +1,41 @@
 import React, { ReactNode } from "react";
 import Navbar from "../Navbar/Navbar";
-import Header from "../Subject/Header";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/clientApp';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const [user, loading, error] = useAuthState(auth); // Firebase auth state
+  const userLoggedIn = user !== null && !loading && !error; // Check if user is logged in
+
   return (
-  <>
-    <Navbar />
-    <main>{children}</main>
-  </>
-)}
+    <>
+      {userLoggedIn ? (
+        <>
+          <Navbar/>
+          <main>{children}</main>
+        </>
+      ) : (
+        <iframe
+          src="/path-to-your-html-file.html"
+          title="Login Required"
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            border: "none",
+            zIndex: 9999, // Adjust z-index as needed
+          }}
+        />
+        // Replace "/path-to-your-html-file.html" with the actual path to your HTML file
+      )}
+    </>
+  );
+};
+
 export default Layout;
