ab742b751d7d209d5218db1ee20a42bff2358e0e
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-06-28 16:18:49 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home = () => {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="welcome.css" />
+        <link rel="stylesheet" href="/Users/sarthakahuja/Downloads/GR8ER/finalsitegr8er/src/pages/welcome.css" />
         <style>{`
           body {
             overflow: hidden; /* Prevent scrolling */
