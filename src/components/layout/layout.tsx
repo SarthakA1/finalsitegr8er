968f17f1d89b2b847bef93276721af7d471689c2
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-06-29 22:16:25 +0530

    Update layout.tsx

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,5 +1,4 @@
 import React, { ReactNode } from "react";
-import { useRouter } from "next/router";
 import Navbar from "../Navbar/Navbar";
 import Header from "../Subject/Header";
 
@@ -8,15 +7,10 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const router = useRouter();
-  const noNavbarRoutes = ["/"]; // Add routes where you don't want to show the Navbar
-
   return (
-    <>
-      {!noNavbarRoutes.includes(router.pathname) && <Navbar />}
-      <main>{children}</main>
-    </>
-  );
-};
-
+  <>
+    <Navbar />
+    <main>{children}</main>
+  </>
+)}
 export default Layout;
