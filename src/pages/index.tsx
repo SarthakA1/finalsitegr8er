1235793a7e73f198c0481a0bf33f8660f4b929d4
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2024-06-29 17:31:31 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -96,9 +96,9 @@ const styles = {
   backgroundShape: css`
     position: absolute;
     top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
+    left: 40;
+    width: 50%;
+    height: 50%;
     opacity: 0.1;
     z-index: 0;
     pointer-events: none;
