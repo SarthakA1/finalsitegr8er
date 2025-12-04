0a2aa46ed53565f5a6e847e05697c8688129c9ed
SarthakA1 <92513582+SarthakA1@users.noreply.github.com>
2025-12-03 20:09:08 -0600

    Layout Update: Maximized Post Visibility (75/25 Split, 1600px Width)

--- src/components/layout/PageContent.tsx
+++ src/components/layout/PageContent.tsx
@@ -9,18 +9,18 @@ const PageContentLayout: React.FC<PageContentProps> = ({ children }) => {
 
     return (
         <Flex justify='center' p="16px 0px" >
-            <Flex width="100%" justify='center' maxWidth='1300px'  >
+            <Flex width="100%" justify='center' maxWidth='1600px'  >
                 {/* {LHS - Main Content} */}
                 <Flex
                     direction='column'
-                    width={{ base: "100%", md: "65%" }}
+                    width={{ base: "100%", md: "75%" }}
                     mr={{ base: 0, md: 6 }}
                 >{children && children[0 as keyof typeof children]}</Flex>
                 {/* {RHS - Sidebar} */}
                 <Flex
                     direction='column'
                     display={{ base: "none", md: "flex" }}
-                    width={{ base: "100%", md: "35%" }}
+                    width={{ base: "100%", md: "25%" }}
                     flexGrow={1}
                 >{children && children[1 as keyof typeof children]}</Flex>
             </Flex>
